ce0bcc8a023a83f4008a5c82e2804bf6408de461
Jonas Gustafson <jonasgustafson@hotmail.se>
2023-06-14 10:36:30 +0200

    forgot to build on last commit

--- src/declarations/hooks/useUpload/useUpload.d.ts
+++ src/declarations/hooks/useUpload/useUpload.d.ts
@@ -4,5 +4,5 @@ type UploadHookReturnType<T = unknown> = [
     (blob: Blob, options?: FetchOptions) => AbortController,
     State<T>
 ];
-export declare function useUpload<T>(url: string, options?: Omit<FetchHookOptions<T>, 'headers'>): UploadHookReturnType<T>;
+export declare function useUpload<T>(url: string, options?: Omit<FetchHookOptions<T>, 'headers' | 'method'>): UploadHookReturnType<T>;
 export {};
